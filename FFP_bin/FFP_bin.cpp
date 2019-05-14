da87b2aedd204773bf136ef460dcdc1bc410028b
JaeJin Choi <desert31@hotmail.com>
2019-05-13 22:36:26 -0700

    Update FFP_bin.cpp

--- FFP_bin/FFP_bin.cpp
+++ FFP_bin/FFP_bin.cpp
@@ -718,7 +718,7 @@ void show_help()
 
 void show_profile()
 {
-    cout << "FFP binary version; update 2018-9-21\n";
+    cout << "FFP binary version 2v.2.1 (2018-9-21)\n";
     cout << "Value presentation: a point below 4 decimal places (%.4e)\n";
 
     cout << "Code by JaeJin Choi; https://github.com/jaejinchoi/FFP\n";
